78f7d36071262a04be88494a6af8d35507d8d74f
YusufAkilevi <yusufaklevi@gmail.com>
2023-09-29 17:36:14 +0300

    ProductDetailPage routing added

--- e-commerce-project/src/components/Products/ProductCard.jsx
+++ e-commerce-project/src/components/Products/ProductCard.jsx
@@ -4,29 +4,24 @@ import { Link } from "react-router-dom";
 import Button from "../UI/Button";
 const ProductCard = ({ product }) => {
   return (
-    <Link to={`/${product.id}`}>
-      <div
-        key={product.id}
-        className=" flex flex-col group relative border rounded-md p-6 cursor-pointer"
-      >
+    <div
+      key={product.id}
+      className=" flex flex-col border rounded-md p-6 cursor-pointer"
+    >
+      <Link to={`/${product.category}/${product.id}`}>
         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
           <img
+            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
             src={product.thumbnail}
             alt={product.title}
-            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
           />
         </div>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <span aria-hidden="true" className="absolute inset-0" />
-              {product.title}
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">{product.brand}</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">${product.price}</p>
+        <div className="mt-4">
+          <h3 className="text-sm text-gray-700">{product.title}</h3>
+          <p className=" text-sm text-gray-500">{product.brand}</p>
         </div>
-        <div className="flex items-end gap-1.5 text-gray-500 text-sm">
+
+        <div className="flex items-end gap-1.5 text-gray-500 text-sm mt-1">
           <Rating
             size={20}
             readonly
@@ -35,11 +30,16 @@ const ProductCard = ({ product }) => {
           />
           <span>{product.rating}</span>
         </div>
-        <div className="mt-4 w-full">
-          <Button className="w-full">Add to Cart</Button>
-        </div>
+        <p className="text-sm font-medium text-gray-900 mt-2 mb-2">
+          ${product.price}
+        </p>
+      </Link>
+      <div className="mt-auto w-full">
+        <Button className="w-full hover:bg-gray-400 hover:text-white">
+          Add to Cart
+        </Button>
       </div>
-    </Link>
+    </div>
   );
 };
 
---
+++ e-commerce-project/src/components/Products/ProductDetailComp.jsx
@@ -0,0 +1,5 @@
+const ProductDetailComp = ({ product }) => {
+  console.log(product);
+  return <div>{}</div>;
+};
+export default ProductDetailComp;
