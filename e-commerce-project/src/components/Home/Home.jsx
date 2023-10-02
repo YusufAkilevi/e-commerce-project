3b538ffd1b228c0984263ecfdf8bae22a9608754
YusufAkilevi <yusufaklevi@gmail.com>
2023-10-02 20:19:41 +0300

    bug fix

--- e-commerce-project/src/components/Home/Home.jsx
+++ e-commerce-project/src/components/Home/Home.jsx
@@ -1,8 +1,4 @@
-import { useEffect, useState } from "react";
 import ProductList from "../Products/ProductList";
-import Loading from "../UI/Loading";
-import { useFetch } from "../../utils/util";
-import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
   return <ProductList />;
--- e-commerce-project/src/pages/Category.jsx
+++ e-commerce-project/src/pages/Category.jsx
@@ -1,7 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useFetch } from "../utils/util";
-import ProductList from "../components/Products/ProductList";
-import Loading from "../components/UI/Loading";
+
 import CategoryProductList from "../components/Products/CategoryProductList";
 const Category = () => {
   const { categoryName } = useParams();
